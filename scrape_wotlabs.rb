e2297fee27ce6b45ce3d0756c0b7726d2f3b5615
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-09-17 10:55:24 +0900

    実装を整理

--- scrape_wotlabs.rb
+++ scrape_wotlabs.rb
@@ -4,23 +4,22 @@ require 'net/http'
 # Nokogiriライブラリの読み込み
 require 'nokogiri'
 
-def scrape(name)
-  p name
-  charset = nil
-  html = Net::HTTP.get('wotlabs.net', '/sea/player/' + name) do |f|
-    charset = f.charset # 文字種別を取得
-    f.read # htmlを読み込んで変数htmlに渡す
-  end
-  # htmlをパース(解析)してオブジェクトを生成
-  doc = Nokogiri::HTML.parse(html, nil, charset)
-  table = doc.xpath('//*[@id="tankerStats"]/table[@class="gridtable"]')[2]
-  text = ""
-  table.css('tr').each do |node|
-    text = text + node.css('td')[0].inner_text + ":" + node.css('td')[1].inner_text + ", "
-  end
-  p text
+def printTable(table)
+  p table.css('tr').map{|node| node.css('td')[1].inner_text}.slice(1, 14).join("\x09")
 end
 
+def getTable(doc)
+  return doc.xpath('//*[@id="tankerStats"]/table[@class="gridtable generalStats"]')
+end
+
+def scrape(name)
+  p name
+  html = Net::HTTP.get('wotlabs.net', '/sea/player/' + name)
+  doc = Nokogiri::HTML(html)
+  printTable(getTable(doc))
+end
+
+# ['bloodysnowx'].each do |name|
 ['bloodysnowx', 'ehou_maki', 'khiro256', 'LOHZ', 'mamastan', 'cocota24', 
  'DForrester', 'watashiehou', 'mikoshi_fighter', 'okaotank'].each do |name|
   scrape(name)
