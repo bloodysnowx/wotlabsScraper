8a24954dc16464ba917f67aac52f77018411e8cf
bloodysnowx <bloodysnowx@gmail.com>
2014-09-23 15:17:08 +0900

    データ追加

--- scrape_wotlabs.rb
+++ scrape_wotlabs.rb
@@ -23,6 +23,6 @@ end
 # ['bloodysnowx'].each do |name|
 ['bloodysnowx', 'ehou_maki', 'khiro256', 'LOHZ', 'mamastan', 'cocota24', 
  'DForrester', 'watashiehou', 'mikoshi_fighter', 'okaotank', 'lower_animals',
- 'wilkatz'].each do |name|
+ 'willkatz'].each do |name|
   scrape(name)
 end
